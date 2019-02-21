fc1a7ac16d893e05a3e5189f038c24160c880b7a
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2019-02-21 18:02:15 +0100

    G part is ready

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const body = document.body;
-let header;
-let main;
+const main = document.querySelector(".main");
+const start = document.querySelector(".main__button");
 
 let userAnswers = [];
 let correctAnswers = [];
@@ -32,6 +31,7 @@ let game = {
     this.unanswered = 0;
   }
 };
+
 let score = {
   gamesPlayed: 0,
   correct: 0,
@@ -67,124 +67,32 @@ let score = {
   }
 };
 
-init();
+let data = [];
+getData();
+trapFocus(document.querySelector(".dialog"));
 
-function init() {
-  body.innerHTML = "";
+start.addEventListener("click", startQuiz);
+document.querySelector(".main__submit").addEventListener("click", check);
+document.querySelector(".restart").addEventListener("click", restart);
+document.querySelector(".close").addEventListener("click", close);
 
-  header = document.createElement("header");
-  main = document.createElement("main");
-  body.appendChild(header);
-  body.appendChild(main);
+function startQuiz() {
+  document.querySelector(".main__questions-container").innerHTML = "";
+  game.reset();
 
-  let menuBtn = document.createElement("button");
-  menuBtn.setAttribute("class", "material-icons header__menu_button");
-  menuBtn.textContent = "menu";
-  header.appendChild(menuBtn);
+  if (start.classList.contains("main__button--visible") && !main.classList.contains("main--visible")) {
+    start.classList.toggle("main__button--visible");
+    main.classList.toggle("main--visible");
+  }
 
-  let h1 = document.createElement("h1");
-  h1.classList.add("header__title");
-  h1.textContent = "Quiz Master";
-  header.appendChild(h1);
+  document.querySelector(".main__title").textContent = "Quiz " + (score.getGamesPlayed() + 1);
 
-  let startBtn = document.createElement("button");
-  startBtn.classList.add("main__startQuiz");
-  startBtn.textContent = "START QUIZ";
-  startBtn.addEventListener("click", start);
-  main.appendChild(startBtn);
+  createQuestions();
+
+  getData();
 }
 
-function start() {
-  main.innerHTML = "";
-
-  getQuestions().then(function(result) {
-    let data = JSON.parse(result);
-
-    if (data.response_code !== 0) {
-      let error = document.createElement("h2");
-      error.classList.add("main__error");
-      error.textContent = "Could not get questions from server!";
-      main.appendChild(error);
-    } else {
-      let questions = data.results;
-
-      game.reset();
-
-      let title = document.createElement("h2");
-      title.classList.add("main__title");
-      title.textContent = "Quiz " + (score.getGamesPlayed() + 1);
-      main.appendChild(title);
-
-      let qNumber = 0;
-      let qAnswers = [];
-      for (let question of questions) {
-        qNumber++;
-
-        let q = document.createElement("div");
-        q.classList.add("question");
-
-        let qText = document.createElement("p");
-        qText.classList.add("question__text");
-        qText.textContent = "Q" + qNumber + ". " + decode(question.question);
-        q.appendChild(qText);
-
-        correctAnswers.push(decode(question.correct_answer));
-        qAnswers.push(decode(question.correct_answer));
-        for (let incorrect of question.incorrect_answers) {
-          qAnswers.push(decode(incorrect));
-        }
-
-        while (qAnswers.length > 0) {
-          let i = Math.floor(Math.random() * qAnswers.length);
-
-          let container = document.createElement("div");
-          container.classList.add("question__container");
-
-          let answer = document.createElement("input");
-          answer.classList.add("question__answer");
-          answer.id = qAnswers[i];
-          answer.type = "radio";
-          answer.name = "q" + qNumber;
-          answer.dataset.pos = qNumber - 1;
-          answer.value = qAnswers[i];
-          container.appendChild(answer);
-
-          let label = document.createElement("label");
-          label.classList.add("question__answerText")
-          label.textContent = qAnswers[i];
-          label.for = qAnswers[i];
-          container.appendChild(label);
-
-          q.appendChild(container);
-          qAnswers.splice(i, 1);
-        }
-
-        qAnswers = [];
-
-        main.appendChild(q);
-      }
-
-      let submit = document.createElement("button");
-      submit.classList.add("main__submit");
-      submit.textContent = "SUBMIT!";
-      submit.addEventListener("click", results);
-      main.appendChild(submit);
-    }
-  });
-}
-
-function getQuestions() {
-  return new Promise(function(resolve, reject) {
-    let req = new XMLHttpRequest();
-    req.open("GET", "https://opentdb.com/api.php?amount=10&type=multiple");
-    req.onload = function() {
-      resolve(req.responseText);
-    };
-    req.send();
-  });
-}
-
-function results() {
+function check() {
   let answers = document.querySelectorAll(".question__answer:checked");
 
   score.increaseGamesPlayed();
@@ -200,23 +108,15 @@ function results() {
     }
   }
 
-  createDialog();
+  showResult();
 }
 
-function createDialog() {
-  let dialogContainer = document.createElement("div");
-  dialogContainer.classList.add("container");
+function showResult() {
+  document.querySelector(".dialog").classList.add("dialog--visible");
 
-  let bg = document.createElement("div");
-  bg.classList.add("container__bg");
-  dialogContainer.appendChild(bg);
-
-  let dialog = document.createElement("div");
-  dialog.classList.add("dialog");
-
-  let dialogTitle = document.createElement("h2");
-  dialogTitle.classList.add("dialog__title");
   let correct = game.getCorrect();
+
+  let dialogTitle = document.querySelector(".dialog__title");
   if (correct >= 8) {
     dialogTitle.textContent = "Congratulations!";
   } else if (correct >= 5 && correct < 8) {
@@ -226,40 +126,111 @@ function createDialog() {
   } else {
     dialogTitle.textContent = "Unlucky!";
   }
-  dialog.appendChild(dialogTitle);
 
-  let dialogText = document.createElement("p");
-  dialogText.classList.add("dialog__text");
-  dialogText.textContent = `You answered ${correct}/10 questions correct!`;
-  dialog.appendChild(dialogText);
+  document.querySelector(".dialog__content").textContent = `You answered ${correct}/10 questions correct!`;
+}
 
-  let buttons = document.createElement("div");
-  buttons.classList.add("dialog__buttons");
-  dialog.appendChild(buttons);
+function restart() {
+  document.querySelector(".dialog").classList.remove("dialog--visible");
+  startQuiz();
+}
 
-  let restart = document.createElement("button");
-  restart.classList.add("dialog__re_start");
-  restart.textContent = "RE-START";
-  restart.addEventListener("click", function() {
-    body.removeChild(body.lastChild);
-    start();
+function close() {
+  document.querySelector(".dialog").classList.remove("dialog--visible");
+  main.classList.remove("main--visible");
+  start.classList.add("main__button--visible");
+}
+
+function createQuestions() {
+  let qNumber = 0;
+  let qAnswers = [];
+  for (let obj of data) {
+    qNumber++;
+
+    let question = document.createElement("div");
+    question.setAttribute("class", "question question--sm");
+
+    let qText = document.createElement("h3");
+    qText.classList.add("question__text");
+    qText.textContent = "Q" + qNumber + ". " + decode(obj.question);
+    question.appendChild(qText);
+
+    correctAnswers.push(decode(obj.correct_answer));
+    qAnswers.push(decode(obj.correct_answer));
+    for (let incorrect of obj.incorrect_answers) {
+      qAnswers.push(decode(incorrect));
+    }
+
+    while (qAnswers.length > 0) {
+      let i = Math.floor(Math.random() * qAnswers.length);
+
+      let container = document.createElement("div");
+      container.classList.add("question__answers");
+
+      let answer = document.createElement("input");
+      answer.classList.add("question__answer");
+      answer.id = qAnswers[i];
+      answer.type = "radio";
+      answer.name = "q" + qNumber;
+      answer.dataset.pos = qNumber - 1;
+      answer.value = qAnswers[i];
+      container.appendChild(answer);
+
+      let label = document.createElement("label");
+      label.classList.add("question__answerText")
+      label.textContent = qAnswers[i];
+      label.for = qAnswers[i];
+      container.appendChild(label);
+
+      question.appendChild(container);
+      main.querySelector(".main__questions-container").appendChild(question);
+      qAnswers.splice(i, 1);
+    }
+  }
+}
+
+function getData() {
+  new Promise(function(resolve, reject) {
+    let req = new XMLHttpRequest();
+    req.open("GET", "https://opentdb.com/api.php?amount=10&type=multiple");
+    req.onload = function() {
+      resolve(req.responseText);
+    };
+    req.send();
+  }).then(function(result) {
+    data = JSON.parse(result).results;
   });
-  buttons.appendChild(restart);
-
-  let close = document.createElement("button");
-  close.classList.add("dialog__close");
-  close.textContent = "CLOSE";
-  close.addEventListener("click", function() {
-    body.removeChild(body.lastChild);
-    init();
-  });
-  buttons.appendChild(close);
-
-  dialogContainer.appendChild(dialog);
-  body.appendChild(dialogContainer);
 }
 
 function decode(input) {
   let doc = new DOMParser().parseFromString(input, "text/html");
   return doc.documentElement.textContent;
+}
+
+function trapFocus(element) {
+  let focusableEls = element.querySelectorAll(".dialog__button");
+  let firstFocusableEl = focusableEls[0]
+  let lastFocusableEl = focusableEls[focusableEls.length - 1];
+  let KEYCODE_TAB = 9;
+
+  element.addEventListener('keydown', function(e) {
+    let isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) /* shift + tab */ {
+      if (document.activeElement === firstFocusableEl) {
+        lastFocusableEl.focus();
+        e.preventDefault();
+      }
+    } else /* tab */ {
+      if (document.activeElement === lastFocusableEl) {
+        firstFocusableEl.focus();
+        e.preventDefault();
+      }
+    }
+
+  });
 }
--- src/elements/mainTitle.js
+++
@@ -1,9 +0,0 @@
-export default {
-  init: function(className, text) {
-    let h1 = document.createElement("h1");
-    h1.classList.add(className);
-    h1.textContent = text;
-
-    return h1;
-  }
-};
